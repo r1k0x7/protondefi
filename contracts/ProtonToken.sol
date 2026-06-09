e3a35234ca8f4bb02cfa983fece5fe66e9916920
r1k0x7 <yun798731@gmail.com>
2026-06-10 06:31:35 +0700

    Update ProtonToken.sol

--- contracts/ProtonToken.sol
+++ contracts/ProtonToken.sol
@@ -1,3 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ProtonToken is ERC20, Ownable {
+    uint256 public constant MAX_SUPPLY = 100_000_000 * 10**18; // 100M tokens
+
+    constructor() ERC20("Proton Token", "PROTON") {
+        _mint(msg.sender, 10_000_000 * 10**18); // 10M initial supply
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        require(totalSupply() + amount <= MAX_SUPPLY, "Max supply exceeded");
+        _mint(to, amount);
+    }
+}
 
 
